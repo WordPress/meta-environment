ea3c988e6ef96a265e4815d4f9ab7bb96321c688
Ian Dunn <ian@iandunn.name>
2014-06-22 15:49:02 -0700

    Add custom MySQL init script

---
+++ database/init-custom.sql
@@ -0,0 +1,10 @@
+# Any SQL files included in the database/backups directory will be
+# imported as Vagrant boots up. To best manage expectations, these
+# databases should be created in advance with proper user permissions
+# so that any code bases configured to work with them will start
+# without trouble.
+
+DROP DATABASE IF EXISTS `test`;
+
+CREATE DATABASE IF NOT EXISTS `wordcamp_dev`;
+GRANT ALL PRIVILEGES ON `wordcamp_dev`.* TO 'wp_meta'@'localhost' IDENTIFIED BY 'password';
