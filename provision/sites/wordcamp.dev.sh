41316409499113b0672a4ee038b61abc138d8c0c
Ian Dunn <ian@iandunn.name>
2014-06-22 18:26:40 -0700

    Copy mu-plugins after wp-content has been created.
    Otherwise the directory doesn't exist.

--- provision/sites/wordcamp.dev.sh
+++ provision/sites/wordcamp.dev.sh
@@ -6,10 +6,8 @@ if [ ! -d $SITE_DIR ]; then
 	printf "\nProvisioning wordcamp.dev\n"
 
 	# Setup WordPress
-	mkdir $SITE_DIR/wp-content/mu-plugins
 	wp core download --path=$SITE_DIR/wordpress
 	cp /vagrant/config/wordpress-config/sites/wordcamp.dev/wp-config.php                   $SITE_DIR
-	cp /vagrant/config/wordpress-config/sites/wordcamp.dev/sandbox-functionality.php       $SITE_DIR/wp-content/mu-plugins/
 
 	# Check out WordCamp.org source code
 	svn co https://meta.svn.wordpress.org/sites/trunk/wordcamp.org/public_html/wp-content/ $SITE_DIR/wp-content
@@ -17,6 +15,10 @@ if [ ! -d $SITE_DIR ]; then
     svn co https://plugins.svn.wordpress.org/camptix-network-tools/trunk/                  $SITE_DIR/wp-content/plugins/camptix-network-tools
 	svn co https://plugins.svn.wordpress.org/email-post-changes/trunk/                     $SITE_DIR/wp-content/plugins/email-post-changes
 	svn co https://plugins.svn.wordpress.org/tagregator/trunk/                             $SITE_DIR/wp-content/plugins/tagregator
+
+	# Setup mu-plugin for local development
+	mkdir $SITE_DIR/wp-content/mu-plugins
+	cp /vagrant/config/wordpress-config/sites/wordcamp.dev/sandbox-functionality.php       $SITE_DIR/wp-content/mu-plugins/
 
 	# Install 3rd-party plugins
 	PLUGINS=( akismet buddypress bbpress camptix-pagseguro camptix-payfast-gateway core-control debug-bar debug-bar-console debug-bar-cron jetpack wp-multibyte-patch wordpress-importer )
