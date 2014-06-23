2b54d6bc2bc43b61caacc2a70b4e23a364c76d81
Ian Dunn <ian@iandunn.name>
2014-06-23 09:01:25 -0700

    Fix whitespace tab vs space inconsistency

--- config/wordpress-config/sites/wordcamp.dev/wp-config.php
+++ config/wordpress-config/sites/wordcamp.dev/wp-config.php
@@ -4,29 +4,29 @@ $table_prefix  = 'wp_';
 
 define( 'IS_WORDCAMP_NETWORK',  true );
 
-define( 'DB_NAME',              'wordcamp_dev' );
-define( 'DB_USER',              'wp_meta' );
-define( 'DB_PASSWORD',          'password' );
-define( 'DB_HOST',              'localhost' );
-define( 'DB_CHARSET',           'utf8' );
-define( 'DB_COLLATE',           '' );
+define( 'DB_NAME',               'wordcamp_dev' );
+define( 'DB_USER',               'wp_meta' );
+define( 'DB_PASSWORD',           'password' );
+define( 'DB_HOST',               'localhost' );
+define( 'DB_CHARSET',            'utf8' );
+define( 'DB_COLLATE',            '' );
 
-ini_set( 'log_errors',			'On' );
-ini_set( 'display_errors',		'On' );
-ini_set( 'error_reporting',		E_ALL );
+ini_set( 'log_errors',           'On' );
+ini_set( 'display_errors',       'On' );
+ini_set( 'error_reporting',      E_ALL );
 
-define( 'WP_DEBUG',				true );
-define( 'WP_DEBUG_LOG',			true );
-define( 'WP_DEBUG_DISPLAY',		true );
-define( 'SAVEQUERIES',			true );
-define( 'JETPACK_DEV_DEBUG',	true );
-define( 'SCRIPT_DEBUG',			true );
+define( 'WP_DEBUG',              true );
+define( 'WP_DEBUG_LOG',          true );
+define( 'WP_DEBUG_DISPLAY',      true );
+define( 'SAVEQUERIES',           true );
+define( 'JETPACK_DEV_DEBUG',     true );
+define( 'SCRIPT_DEBUG',          true );
 
-define( 'WP_ALLOW_MULTISITE',   true );
-define( 'MULTISITE',            true );
-define( 'SUBDOMAIN_INSTALL',    true );
-define( 'DOMAIN_CURRENT_SITE',  'wordcamp.dev' );
-define( 'PATH_CURRENT_SITE',    '/' );
+define( 'WP_ALLOW_MULTISITE',    true );
+define( 'MULTISITE',             true );
+define( 'SUBDOMAIN_INSTALL',     true );
+define( 'DOMAIN_CURRENT_SITE',   'wordcamp.dev' );
+define( 'PATH_CURRENT_SITE',     '/' );
 define( 'SITE_ID_CURRENT_SITE',  1 );
 define( 'BLOG_ID_CURRENT_SITE',  2 );	// central.wordcamp.dev
 define( 'CLI_HOSTNAME_OVERRIDE', 'wordcamp.dev' );
