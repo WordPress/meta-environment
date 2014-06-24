fbb2eda80b3a858129c7369e47c78422632adeca
Ian Dunn <ian@iandunn.name>
2014-06-23 23:27:35 -0700

    Use dashboard-custom.php instead of modifying VVV index.php.

---
+++ www/default/dashboard-custom.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WordPress Meta Environment Dashboard</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<h3>Meta Sites</h3>
+
+<ul class="nav">
+	<li><a href="http://developer.wordpress.dev">developer.wordpress.dev</a></li>
+	<li><a href="http://wordcamp.dev">wordcamp.dev</a></li>
+</ul>
+
+<h3>Tools and Resources</h3>
+
+<ul class="nav">
+	<li><a href="phpinfo/">PHP Info</a></li>
+	<li><a href="database-admin/">phpMyAdmin</a></li>
+	<li><a href="memcached-admin/">phpMemcachedAdmin</a></li>
+	<li><a href="opcache-status/opcache.php">Opcache Status</a></li>
+	<li><a href="webgrind/">Webgrind</a></li>
+	<li><a href="https://github.com/iandunn/wordpress-meta-environment/">WordPress Meta Environment</a></li>
+</ul>
+
+</body>
+</html>
--- www/default/index.php
+++ www/default/index.php
@@ -14,26 +14,25 @@ if ( file_exists( 'dashboard-custom.php' ) ) {
 <!DOCTYPE html>
 <html>
 <head>
-	<title>WordPress Meta Environment Dashboard</title>
+	<title>Varying Vagrant Vagrants Dashboard</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-
-<h3>Meta Sites</h3>
 <ul class="nav">
-	<li><a href="http://developer.wordpress.dev">developer.wordpress.dev</a></li>
-	<li><a href="http://wordcamp.dev">wordcamp.dev</a></li>
-</ul>
-
-<h3>Tools and Resources</h3>
-<ul class="nav">
-	<li><a href="phpinfo/">PHP Info</a></li>
+	<li class="active"><a href="#">Home</a></li>
+	<li><a href="https://github.com/varying-vagrant-vagrants/vvv/">Repository</a></li>
 	<li><a href="database-admin/">phpMyAdmin</a></li>
 	<li><a href="memcached-admin/">phpMemcachedAdmin</a></li>
 	<li><a href="opcache-status/opcache.php">Opcache Status</a></li>
 	<li><a href="webgrind/">Webgrind</a></li>
-	<li><a href="https://github.com/iandunn/wordpress-meta-environment/">WordPress Meta Environment</a></li>
+	<li><a href="phpinfo/">PHP Info</a></li>
 </ul>
 
+<ul class="nav">
+	<li><a href="http://local.wordpress.dev/">http://local.wordpress.dev</a> for WordPress stable (/www/wordpress-default)</li>
+	<li><a href="http://local.wordpress-trunk.dev/">http://local.wordpress-trunk.dev</a> for WordPress trunk (/www/wordpress-trunk)</li>
+	<li><a href="http://src.wordpress-develop.dev/">http://src.wordpress-develop.dev</a> for trunk WordPress development files (/www/wordpress-developer/src)</li>
+	<li><a href="http://build.wordpress-develop.dev/">http://build.wordpress-develop.dev</a> for a Grunt build of those development files (/www/wordpress-developer/build)</li>
+</ul>
 </body>
 </html>
