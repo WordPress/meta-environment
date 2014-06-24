71d1ac88d07f5bcae6a351105ae60f94c112c002
Ian Dunn <ian@iandunn.name>
2014-06-24 12:20:58 -0700

    Run wp-cli as vagrant user

--- provision/provision-post.sh
+++ provision/provision-post.sh
@@ -9,6 +9,11 @@ else
 	gem install sass
 fi
 
+# Run wp-cli as vagrant user
+if (( $EUID == 0 )); then
+    wp() { sudo -EH -u vagrant -- wp "$@"; }
+fi
+
 # Look for site setups scripts in the provision/sites directory too
 for SITE_CONFIG_FILE in $(find /vagrant/provision/sites -maxdepth 5 -name '*.sh'); do
 	source $SITE_CONFIG_FILE
