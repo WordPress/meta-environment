b0afc58a21e506b52bd4ef585c3ab4ad11413e31
Ian Dunn <ian@iandunn.name>
2014-07-08 11:27:16 -0700

    global.wordpress.dev: Stub the Rosetta plugin until it's open-sourced.
    This is necessary to avoid fatal PHP errors.

--- config/wordpress-config/sites/global.wordpress.dev/sandbox-functionality.php
+++ config/wordpress-config/sites/global.wordpress.dev/sandbox-functionality.php
@@ -8,3 +8,22 @@ define( 'WP_CORE_STABLE_BRANCH', '3.9' );
 require_once( __DIR__ . '/downloads/rosetta-downloads.php' );
 require_once( __DIR__ . '/roles/rosetta-roles.php' );
 require_once( __DIR__ . '/showcase/rosetta-showcase.php' );
+
+// Stub the Rosetta plugin until it's open-sourced, to avoid fatal PHP errors
+class Rosetta_WordPress {
+	public $rosetta, $showcase, $locale;
+
+	public function __construct() {
+		$this->locale   = 'en';
+		$this->rosetta  = new Rosetta();
+		$this->showcase = new Rosetta_Showcase();
+	}
+}
+
+class Rosetta {
+	public function get_latest_release() {
+		return WP_CORE_STABLE_BRANCH;
+	}
+}
+
+$GLOBALS['rosetta'] = new Rosetta_WordPress();
