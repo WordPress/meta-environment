12665b2fccd69b1c2309111e8d146437f6443291
Ian Dunn <ian@iandunn.name>
2014-10-08 12:33:54 -0700

    Download WordPress.org header/footer over HTTPS.
    WordPress.org recently switched everything to force SSL, and curl wasn't following the redirect, so it was just downloading Nginx's default 302 page instead of the actual header.
    
    Fixes #2

--- provision/sites/global.wordpress.dev.sh
+++ provision/sites/global.wordpress.dev.sh
@@ -28,5 +28,5 @@ else
 fi
 
 # Pull global header/footer
-curl -o $SITE_DIR/header.php  http://wordpress.org/header.php
+curl -o $SITE_DIR/header.php  https://wordpress.org/header.php
 echo "<?php wp_head(); ?>" >> $SITE_DIR/header.php
