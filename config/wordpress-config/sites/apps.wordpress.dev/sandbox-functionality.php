0eafbe434cd1e342812615cba12eeb48732f60d5
Ian Dunn <ian@iandunn.name>
2014-12-03 18:51:35 -0800

    Remove VVV
    This is the first step in migrating the repository to simply be a collection of provision scripts that runs on top of an existing VVV installation, rather than a complete fork of VVV.

--- config/wordpress-config/sites/apps.wordpress.dev/sandbox-functionality.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-// Include Core themes
-register_theme_directory( ABSPATH . 'wp-content/themes' );
--- config/wordpress-config/sites/developer.wordpress.dev/sandbox-functionality.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Include Core themes
-register_theme_directory( ABSPATH . 'wp-content/themes' );
-
-add_filter( 'show_admin_bar', '__return_false' );
