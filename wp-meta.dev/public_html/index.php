6f141cd2e919d85320bc31f57c605c3cb3bfc886
Ian Dunn <ian@iandunn.name>
2014-12-06 14:43:49 -0800

    Add a dashboard with links to all the sites and other resources.

---
+++ wp-meta.dev/public_html/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>WordPress Meta Environment</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="style.css" type="text/css" media="all" />
+</head>
+
+<body>
+
+	<h1>WordPress Meta Environment</h1>
+	
+	<p>
+		All of the sites listed below have been provisioned into your local development environment. You can log in
+		to any of them with username <code>admin</code> and password <code>password</code>.
+	</p>
+	
+	<h2>Available Sites</h2>
+	<ul>
+		<li><a href="http://apps.wordpress.dev">apps.wordpress.dev</a></li>
+		<li><a href="http://developer.wordpress.dev">developer.wordpress.dev</a></li>
+		<li>
+			<a href="http://global.wordpress.dev">global.wordpress.dev</a>
+
+			<ul>
+				<li><a href="http://es.wordpress.dev">es.wordpress.dev</a></li>
+				<li><a href="http://ja.wordpress.dev">ja.wordpress.dev</a></li>
+			</ul>
+		</li>
+		<li><a href="http://jobs.wordpress.dev">jobs.wordpress.dev</a></li>
+		<li>
+			<a href="http://wordcamp.dev">wordcamp.dev</a>
+
+			<ul>
+				<li><a href="http://central.wordcamp.dev">central.wordcamp.dev</a></li>
+				<li><a href="http://plan.wordcamp.dev">plan.wordcamp.dev</a></li>
+				<li><a href="http://2014.seattle.wordcamp.dev">2014.seattle.wordcamp.dev</a></li>
+				<li><a href="http://wordcamp.dev/wp-admin/network/sites.php">more</a></li>
+			</ul>
+		</li>
+		<li><a href="http://wordpresstv.dev">wordpresstv.dev</a></li>
+	</ul>
+	
+	<h2>Resources</h2>
+	<ul>
+		<li><a href="https://github.com/iandunn/wordpress-meta-environment">WME homepage</a></li>
+		<li><a href="https://make.wordpress.org/meta/handbook/">Meta Team Handbook</a> - for information on contributing to these sites</li>
+		<li><a href="https://meta.trac.wordpress.org">Meta Trac</a> - find a ticket to work on</li>
+		<li><a href="http://vvv.dev">VVV dashboard</a> - phpMyAdmin, phpinfo(), and other tools/resources</li>
+		<li><a href="https://github.com/Varying-Vagrant-Vagrants/VVV">VVV homepage</a> - for information on working with VVV in general</li>
+	</ul>
+
+</body>
+</html>
