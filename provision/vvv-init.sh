4b6b269d6edfb2667c2317b538da8b11d74a2859
Tom J Nowell <contact@tomjn.com>
2019-04-01 21:29:48 +0100

    Install wkhtmlpdf prior to setting up meta sites

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -5,8 +5,23 @@
 
 echo "Beginning WP Meta Environment provisioning"
 
+# Install wkhtmltopdf for wordcamp-docs plugin
+if [ ! -f /usr/local/bin/wkhtmltopdf ];
+then
+  echo "Installing wkhtmltopdf"
+	apt-get install -y wkhtmltopdf xvfb
+	echo 'xvfb-run --server-args="-screen 0, 1024x768x24" /usr/bin/wkhtmltopdf $*' | sudo tee /usr/bin/wkhtmltopdf.sh > /dev/null
+	chmod a+x /usr/bin/wkhtmltopdf.sh
+	ln -s /usr/bin/wkhtmltopdf.sh /usr/local/bin/wkhtmltopdf
+fi
+
+echo "Setting up buddypressorg.test"
 (../buddypressorg.test/provision/vvv-init.sh)
+
+echo "Setting up jobs.wordpressnet.test"
 (../jobs.wordpressnet.test/provision/vvv-init.sh )
+
+echo "Setting up buddypress.org"
 (../wordcamp.test/provision/vvv-init.sh)
 (../wordpressorg.test/provision/vvv-init.sh)
 (../wordpresstv.test/provision/vvv-init.sh)
