2b93b1d9970c514d575df2aeb8b61b6fe994351f
Ian Dunn <ian@iandunn.name>
2019-04-02 20:48:08 -0700

    Provision: Run provisioners in subshell with absolute path for safety.
    Previously the path would be incorrect in some host environments under some situations. This removes any ambiguity, and isolates each provisioner into its own scope.
    
    Fixes #130

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-#
-# This loads the various provisioners for each site in the meta environment,
-# so that VVV doesn't have to do a folder search to find them
+
+# This loads the various provisioners for each site in the Meta Environment,
+# so that VVV doesn't have to do a folder search to find them.
 
 echo "Beginning WordPress Meta Environment provisioning"
 
-source ../buddypressorg.test/provision/vvv-init.sh
-source ../jobs.wordpressnet.test/provision/vvv-init.sh
-source ../wordcamp.test/provision/vvv-init.sh
-source ../wordpressorg.test/provision/vvv-init.sh
-source ../wordpresstv.test/provision/vvv-init.sh
+BASE_DIR="$( dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd ) )"
+
+# Launch each provisioner in a subshell to avoid conflicting global variable names.
+# For example, `$BASE_DIR` would be overridden by each provisioner.
+( source $BASE_DIR/buddypressorg.test/provision/vvv-init.sh     )
+( source $BASE_DIR/jobs.wordpressnet.test/provision/vvv-init.sh )
+( source $BASE_DIR/wordcamp.test/provision/vvv-init.sh          )
+( source $BASE_DIR/wordpressorg.test/provision/vvv-init.sh      )
+( source $BASE_DIR/wordpresstv.test/provision/vvv-init.sh       )
 
 echo "Completed WordPress Meta Environment provisioning"
