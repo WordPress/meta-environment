21306ea229ba2b8d4b50cfe5ef3d79c4bcb43b13
Tom J Nowell <contact@tomjn.com>
2021-04-14 12:09:46 +0100

    avoid globbing expansion in jobs provisioner variable strings

--- jobs.wordpressnet.test/provision/vvv-init.sh
+++ jobs.wordpressnet.test/provision/vvv-init.sh
@@ -14,25 +14,25 @@ fi
 PROVISION_DIR="$BASE_DIR/$SITE_DOMAIN/provision"
 SITE_DIR="$BASE_DIR/$SITE_DOMAIN/public_html"
 
-wme_svn_git_migration $SITE_DIR
+wme_svn_git_migration "${SITE_DIR}"
 
 if [ ! -L $SITE_DIR ]; then
 	printf "\n#\n# Provisioning $SITE_DOMAIN\n#\n"
 
 	# Don't overwrite existing databases if we're just migrating from SVN to Git
 	if [[ ! $MIGRATED_TO_GIT ]]; then
-		wme_import_database "jobs_wordpressnet_dev" $PROVISION_DIR
+		wme_import_database "jobs_wordpressnet_dev" "${PROVISION_DIR}"
 	fi
 
 	wme_clone_meta_repository $BASE_DIR
-	wme_symlink_public_dir $BASE_DIR $SITE_DOMAIN "jobs.wordpress.net"
-	wme_symlink_logs_dir $BASE_DIR $SITE_DOMAIN "jobs.wordpress.net"
+	wme_symlink_public_dir "${BASE_DIR}" "${SITE_DOMAIN}" "jobs.wordpress.net"
+	wme_symlink_logs_dir "${BASE_DIR}" "${SITE_DOMAIN}" "jobs.wordpress.net"
 
 	# Setup WordPress and plugins
-	wme_noroot wp core download --version=nightly --path=$SITE_DIR/wordpress
-	wme_noroot mkdir -p $SITE_DIR/wp-content/mu-plugins
-	wme_noroot cp -f $PROVISION_DIR/wp-config.php             $SITE_DIR
-	wme_noroot cp -f $PROVISION_DIR/sandbox-functionality.php $SITE_DIR/wp-content/mu-plugins/
+	wme_noroot wp core download --version=nightly --path="${SITE_DIR}/wordpress"
+	wme_noroot mkdir -p "${SITE_DIR}/wp-content/mu-plugins"
+	wme_noroot cp -f "${PROVISION_DIR}/wp-config.php"             "${SITE_DIR}"
+	wme_noroot cp -f "${PROVISION_DIR}/sandbox-functionality.php" "${SITE_DIR}/wp-content/mu-plugins/"
 
 	# Ignore external dependencies and Meta Environment tweaks
 	IGNORED_FILES=(
@@ -46,8 +46,8 @@ if [ ! -L $SITE_DIR ]; then
 else
 	printf "\n#\n# Updating $SITE_DOMAIN\n#\n"
 
-	git -C $SITE_DIR pull origin master
-	wme_noroot wp core   update --version=nightly --path=$SITE_DIR/wordpress
-	wme_noroot wp plugin update --all             --path=$SITE_DIR/wordpress
+	git -C "${SITE_DIR}" pull origin master
+	wme_noroot wp core   update --version=nightly --path="${SITE_DIR}/wordpress"
+	wme_noroot wp plugin update --all             --path="${SITE_DIR}/wordpress"
 
 fi
